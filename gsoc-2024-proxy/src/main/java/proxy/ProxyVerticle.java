47f15877c1930b13ae40c8a8181561c88573cd46
wzy1935 <13053260125@163.com>
2024-03-10 02:23:51 +0100

    proxy

--- gsoc-2024-proxy/src/main/java/proxy/ProxyVerticle.java
+++ gsoc-2024-proxy/src/main/java/proxy/ProxyVerticle.java
@@ -3,6 +3,12 @@ package proxy;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.proxy.handler.ProxyHandler;
+import io.vertx.httpproxy.HttpProxy;
 
 public class ProxyVerticle extends AbstractVerticle {
 
@@ -15,6 +21,19 @@ public class ProxyVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
+    HttpServer proxyServer = vertx.createHttpServer();
+    Router proxyRouter = Router.router(vertx);
+
+    HttpClient proxyClient = vertx.createHttpClient();
+    HttpProxy beProxy = HttpProxy.reverseProxy(proxyClient);
+    beProxy.origin(8080, "localhost");
+    HttpProxy feProxy = HttpProxy.reverseProxy(proxyClient);
+    feProxy.origin(80, "localhost");
+
+    proxyRouter.route("/").handler(ProxyHandler.create(feProxy));
+    proxyRouter.route("/joke").handler(ProxyHandler.create(beProxy));
+
+    proxyServer.requestHandler(proxyRouter).listen(5000);
     startPromise.complete();
   }
 }
