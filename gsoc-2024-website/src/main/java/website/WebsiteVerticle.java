342bde241aca9c0fce181b55d10d60251bac8575
ramanadas <104691675+ramanadas@users.noreply.github.com>
2024-03-18 20:56:55 +0530

    gsoc assignment
    i am ramanadas dhanisetti. i completed assignment. please check it out.

--- gsoc-2024-website/src/main/java/website/WebsiteVerticle.java
+++ gsoc-2024-website/src/main/java/website/WebsiteVerticle.java
@@ -3,6 +3,9 @@ package website;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class WebsiteVerticle extends AbstractVerticle {
 
@@ -15,6 +18,10 @@ public class WebsiteVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
+    HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+    router.route("/").handler(StaticHandler.create());
+    server.requestHandler(router).listen(3000);
     startPromise.complete();
   }
 }
