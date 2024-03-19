8aec63b0f611ead302a2af57c76aebd01da8c33e
ramanadas <104691675+ramanadas@users.noreply.github.com>
2024-03-20 01:06:53 +0530

    Update ProxyVerticle.java
    updated proxy for frontend also.

--- gsoc-2024-proxy/src/main/java/proxy/ProxyVerticle.java
+++ gsoc-2024-proxy/src/main/java/proxy/ProxyVerticle.java
@@ -7,7 +7,6 @@ import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.ext.web.proxy.handler.ProxyHandler;
 import io.vertx.httpproxy.HttpProxy;
 
@@ -27,19 +26,30 @@ public class ProxyVerticle extends AbstractVerticle {
     HttpProxy httpProxy1 = HttpProxy.reverseProxy(proxyClient);
     httpProxy1.origin(8080, "localhost");
 
+    HttpProxy httpProxy2 = HttpProxy.reverseProxy(proxyClient);
+    httpProxy2.origin(3000, "localhost");
+
     HttpServer proxyServer = vertx.createHttpServer();
     Router proxyRouter = Router.router(vertx);
-    proxyRouter.route()
-        .handler(
-          CorsHandler.create()
-            .addRelativeOrigin("http://localhost:3000")
-            .allowedMethod(HttpMethod.GET)
-        );
+
     proxyRouter
       .route(HttpMethod.GET, "/joke").handler(ProxyHandler.create(httpProxy1));
+    proxyRouter
+      .route("/").handler(ProxyHandler.create(httpProxy2));
 
     proxyServer.requestHandler(proxyRouter);
-    proxyServer.listen(7070);
-    startPromise.complete();
+    proxyServer.listen(7070)
+      .onSuccess(h->{
+        startPromise.complete();
+      })
+      .onFailure(h->{
+        startPromise.fail(h.getCause());
+      });
+    startPromise.future().onSuccess(h->{
+      System.out.println("proxy server deployed successfully");
+    })
+      .onFailure(h->{
+        System.out.println("failed to deploy proxy server: " + h.getCause());
+      });
   }
 }
