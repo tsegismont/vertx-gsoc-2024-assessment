11282bec112396444d1ce2c3b83066fd9ea4361f
ramanadas <104691675+ramanadas@users.noreply.github.com>
2024-03-19 17:34:49 +0530

    Proxy
    proxy server port is 7070

--- gsoc-2024-proxy/src/main/java/proxy/ProxyVerticle.java
+++ gsoc-2024-proxy/src/main/java/proxy/ProxyVerticle.java
@@ -3,6 +3,13 @@ package proxy;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
+import io.vertx.ext.web.proxy.handler.ProxyHandler;
+import io.vertx.httpproxy.HttpProxy;
 
 public class ProxyVerticle extends AbstractVerticle {
 
@@ -15,6 +22,24 @@ public class ProxyVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
+    HttpClient proxyClient = vertx.createHttpClient();
+
+    HttpProxy httpProxy1 = HttpProxy.reverseProxy(proxyClient);
+    httpProxy1.origin(8080, "localhost");
+
+    HttpServer proxyServer = vertx.createHttpServer();
+    Router proxyRouter = Router.router(vertx);
+    proxyRouter.route()
+        .handler(
+          CorsHandler.create()
+            .addRelativeOrigin("http://localhost:3000")
+            .allowedMethod(HttpMethod.GET)
+        );
+    proxyRouter
+      .route(HttpMethod.GET, "/joke").handler(ProxyHandler.create(httpProxy1));
+
+    proxyServer.requestHandler(proxyRouter);
+    proxyServer.listen(7070);
     startPromise.complete();
   }
 }
